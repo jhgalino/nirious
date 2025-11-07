ae0302936d2b35c0588d15d07bd06d8be83f2835
John Henry Galino <github@jhgalino.com>
2025-11-07 00:03:14 +0000

    add niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
+dnf5 install -y lightdm
+dnf5 install -y podman distrobox
+dnf5 install -y niri xwayland-satellite
 
 # Use a COPR Example:
 #
@@ -22,3 +25,4 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable lightdm
