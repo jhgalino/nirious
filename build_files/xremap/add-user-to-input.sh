00976459eedc295c35298e6613bf3a1b369399de
jhgalino <38009312+jhgalino@users.noreply.github.com>
2025-11-11 02:16:17 +0800

    Update add-user-to-input.sh

--- build_files/xremap/add-user-to-input.sh
+++ build_files/xremap/add-user-to-input.sh
@@ -1,1 +1,2 @@
 grep -E '^input:' /usr/lib/group | sudo tee -a /etc/group && sudo usermod -aG input $USER
+echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/input.rules
